7802347b14fa9c738b8ca0913e3464f0363f49a2
unknown <andy0n7@gmail.com>
2019-10-13 02:03:30 +0300

    added google pubsub

---
+++ node-app/services/pubsub/apollo.js
@@ -0,0 +1,5 @@
+const { PubSub } = require('apollo-server')
+
+const pubsubApollo = new PubSub()
+
+export { pubsubApollo }
---
+++ node-app/services/pubsub/google.js
@@ -0,0 +1,15 @@
+const { GooglePubSub } = require('@axelspringer/graphql-google-pubsub')
+
+const options = {
+    projectId: process.env.GOOGLE_PUBSUB_PROJECT_ID,
+    credentials: {
+        client_email: process.env.GOOGLE_PUBSUB_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PUBSUB_PRIVATE_KEY
+    }
+}
+const topic2SubName = (topicName) => `${topicName}-subscription`
+const commonMessageHandler = ({attributes = {}, data = ''}) => JSON.parse(data.toString())
+
+const pubsubGoogle = new GooglePubSub(options, topic2SubName, commonMessageHandler)
+
+export { pubsubGoogle }
